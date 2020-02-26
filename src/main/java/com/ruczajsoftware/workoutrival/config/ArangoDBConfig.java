61b879fae8fa9edf551612f54988e21125f80182
PureFour <daniel.baziak@gmail.com>
2020-02-26 22:20:28 +0100

    docker fixes...

--- src/main/java/com/ruczajsoftware/workoutrival/config/ArangoDBConfig.java
+++ src/main/java/com/ruczajsoftware/workoutrival/config/ArangoDBConfig.java
@@ -1,23 +1,67 @@
 package com.ruczajsoftware.workoutrival.config;
 
 import com.arangodb.ArangoDB;
+import com.arangodb.ArangoDatabase;
+import com.arangodb.entity.CollectionEntity;
 import com.arangodb.springframework.annotation.EnableArangoRepositories;
-import com.arangodb.springframework.config.AbstractArangoConfiguration;
+import com.arangodb.springframework.config.ArangoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableArangoRepositories(basePackages = { "com.ruczajsoftware.workoutrival" })
-public class ArangoDBConfig extends AbstractArangoConfiguration {
-	@Override
-	public ArangoDB.Builder arango() {
-		return new ArangoDB.Builder()
-			.host("localhost", 8529)
-			.user("root")
-			.password("root");
-	}
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-	@Override
-	public String database() {
-		return "WorkoutRivalDB";
-	}
+@Configuration
+@EnableArangoRepositories(basePackages = {"com.ruczajsoftware.workoutrival"})
+public class ArangoDBConfig implements ArangoConfiguration {
+
+    private static final String DB_NAME = "workout-rival-db";
+    private static final Set<String> COLLECTIONS = Set.of("Users", "Exercises", "Trainings");
+
+    @Value("${arangodb.host}")
+    private String host;
+
+    @Value("${arangodb.port}")
+    private Integer port;
+
+
+    @Bean
+    public ArangoDB.Builder arango() {
+        return new ArangoDB.Builder()
+                .host(host, port)
+                .user("root")
+                .password("root");
+    }
+
+    @Bean
+    public String database() {
+        return DB_NAME;
+    }
+
+    @PostConstruct
+    public void initDatabase() {
+        ArangoDB arangoDB = arango().build();
+        if (!arangoDB.getDatabases().contains(DB_NAME)) {
+            arangoDB.createDatabase(DB_NAME);
+        }
+        ArangoDatabase database = arangoDB.db(DB_NAME);
+        initCollections(database);
+    }
+
+    private void initCollections(ArangoDatabase database) {
+        final List<String> dbCollectionsNames = database.getCollections().stream()
+                .map(CollectionEntity::getName)
+                .collect(Collectors.toList());
+
+        COLLECTIONS.forEach(
+                collectionName -> {
+                    if (!dbCollectionsNames.contains(collectionName)) {
+                        database.createCollection(collectionName);
+                    }
+                }
+        );
+    }
 }
