e3f6ddb73ab0aa96703a95fc17b3348592a40ac5
PureFour <daniel.baziak@gmail.com>
2020-02-29 22:25:03 +0100

    added SpringBootAdmin and Actuator

--- src/main/java/com/ruczajsoftware/workoutrival/WorkoutRivalApplication.java
+++ src/main/java/com/ruczajsoftware/workoutrival/WorkoutRivalApplication.java
@@ -1,8 +1,10 @@
 package com.ruczajsoftware.workoutrival;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAdminServer
 @SpringBootApplication
 public class WorkoutRivalApplication {
 	public static void main(String[] args) {
