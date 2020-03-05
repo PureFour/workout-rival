b01ec3550c36253bf70162610e398ad4b929924a
PureFour <daniel.baziak@gmail.com>
2020-03-05 20:29:57 +0100

    wr-12 setup deployment

--- src/main/java/com/ruczajsoftware/workoutrival/WorkoutRivalApplication.java
+++ src/main/java/com/ruczajsoftware/workoutrival/WorkoutRivalApplication.java
@@ -1,10 +1,8 @@
 package com.ruczajsoftware.workoutrival;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAdminServer
 @SpringBootApplication
 public class WorkoutRivalApplication {
 	public static void main(String[] args) {
