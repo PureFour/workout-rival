e90c218c9516eae38ae2a119524128c1e5cf835c
Daniel Baziak <38511241+PureFour@users.noreply.github.com>
2020-03-21 18:08:08 +0100

    auth models fix (#27)

--- src/main/java/com/ruczajsoftware/workoutrival/model/authentication/AuthenticationRequest.java
+++ src/main/java/com/ruczajsoftware/workoutrival/model/authentication/AuthenticationRequest.java
@@ -11,6 +11,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class AuthenticationRequest {
 
-    private String userLogin;
-    private String userPassword;
+    private String username;
+    private String password;
 }
